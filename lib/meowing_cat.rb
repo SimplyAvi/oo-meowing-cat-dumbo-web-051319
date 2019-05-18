9d3acaf7b65fd871b578d8c34562a8960b4b6b3b
SimplyAvi <a.totaram@gmail.com>
2019-05-18 17:00:57 -0400

    Done.

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,39 @@
-## code your solution here. 
+## code your solution here.
+
+require "pry"
+class Cat
+  attr_accessor :name
+
+
+  # def initilaize(name)
+  #   @name = name
+  #
+  # end
+
+  # def name(name)
+  #
+  #
+  #   maru = Cat.new(name)
+  #   maru.name = "Maru"
+  #   maru
+  #
+  # end
+
+
+
+  def meow
+    puts "meow!"
+  end
+# binding.
+
+  # maru.name
+  # # => "Maru"
+  #
+  # maru.meow
+  # # "meow!"
+  # # => nil
+
+
+
+
+end
